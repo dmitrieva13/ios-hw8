e4c7e67c3c834b035050ef919e68c0bc23699e85
Varvara <varvara@MacBook-Air-Varvara.local>
2022-03-17 23:45:14 +0300

    point 2

--- vadmitrievaPW8/Controllers/MoviesViewController.swift
+++ vadmitrievaPW8/Controllers/MoviesViewController.swift
@@ -9,12 +9,17 @@ import UIKit
 
 class MoviesViewController: UIViewController {
     private let tableView = UITableView()
+    private let apiKey = "554e7ac8416f0775b18600664214853d"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
         configureUI()
+        DispatchQueue.global(qos: .background).async {
+            [weak self] in
+            self?.loadMovies()
+        }
     }
 
     private func configureUI() {
@@ -29,6 +34,16 @@ class MoviesViewController: UIViewController {
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
         tableView.reloadData()
+    }
+    
+    private func loadMovies() {
+        guard let url = URL(string: "https://api.themoviedb.org/3/ discover/movie?api_key=\(apiKey)&language=ruRU") else {
+            return assertionFailure("some problems with url")
+        }
+        let session = URLSession.shared.dataTask(with: URLRequest(url: url), completionHandler: {data, _, _ in
+            
+        })
+        session.resume()
     }
 }
 
